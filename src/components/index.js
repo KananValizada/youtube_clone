0332d8b1a7083a471bd05b24e2f07dc0b84d7879
KananValizada <kenan.veli@yahoo.com>
2023-01-29 02:15:20 +0400

    first push

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import ChannelDetail from "./ChannelDetail";
+import Feed from "./Feed";
+import Navbar from "./Navbar";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
+import Sidebar from "./Sidebar";
+import SearchBar from "./SearchBar";
+import Videos from "./Videos";
+
+export {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+  Sidebar,
+  SearchBar,
+  Videos,
+};
